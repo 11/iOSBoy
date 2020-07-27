14af81e4f80c88f1aa2c09f79c7e43031c7d35c3
Doug Rudolph <drudolph914@gmail.com>
2020-07-27 16:04:18 -0400

    updated build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,16 @@
 # install rustup
+echo "- Installing rustup"
+echo "- Installing wasm32 target"
+echo "- Compiling & Building with webpack"
+echo "******************************************"
+
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh && echo "1"
+echo "✅ Installing rustup"
 
 # setup rustup toolchain to have the wasm target
-rustup target add wasm32-unknown-unknown
+rustup -y target add wasm32-unknown-unknown
+echo "✅ Installing wasm32 target"
 
 # compile and build the project in production mode
 webpack --mode=production
+echo "✅ Compiling & Building with webpack"
