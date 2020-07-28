73a410ad464b2e4d8ed52770aa453a8a99942df9
Doug Rudolph <drudolph914@gmail.com>
2020-07-28 13:20:30 -0400

    test

--- scripts/build_prod.sh
+++ scripts/build_prod.sh
@@ -16,11 +16,12 @@ echo "✅ Configure rust environment"
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 echo "✅ Installing wasm-pack"
 
-rustup target add wasm32-unknown-unknown
-echo "✅ Installing wasm32 target"
-
-cargo build --target wasm32-unknown-unknown
-echo "✅ Compiling and building emu4iOS' emulators to wasm32 target"
-
-webpack --mode=production
-echo "✅ Compiling and building JS to webpack production target"
+ls
+# rustup target add wasm32-unknown-unknown
+# echo "✅ Installing wasm32 target"
+#
+# cargo build --target wasm32-unknown-unknown
+# echo "✅ Compiling and building emu4iOS' emulators to wasm32 target"
+#
+# webpack --mode=production
+# echo "✅ Compiling and building JS to webpack production target"
