dfa51834fe8d8784ccfc62aecf0222719b11c346
Doug Rudolph <drudolph914@gmail.com>
2020-07-28 12:51:48 -0400

    small change to dev build script

--- scripts/build_dev.sh
+++ scripts/build_dev.sh
@@ -4,8 +4,8 @@ echo "**********************************************************"
 
 if [ ! -d "./pkg/" ]; then
   cargo build --target wasm32-unknown-unknown
-  echo "✅ Compiling and building emu4iOS' emulators to wasm32 target"
 fi
+echo "✅ Compiling and building emu4iOS' emulators to wasm32 target"
 
 webpack --mode=production
 echo "✅ Compiling & Building with webpack"
